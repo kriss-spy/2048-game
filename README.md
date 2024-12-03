607dd68f2d779781a0cfe928d8509ed8b4d74bc0
kriss-spy <3574725417@qq.com>
2024-12-03 17:32:55 +0800

    try to fix rollin

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # 2048-game
+
 side project, learning programming by developing 2048 game
+
+refering to the [hackinscience exercise](https://www.hackinscience.org/exercises/make-your-own-2048-in-python)
--- cli.py
+++ cli.py
@@ -1,20 +1,24 @@
 import numpy as np
 import random
+
+
 def init_grid():
-    grid = np.zeros((4,4))
+    grid = np.zeros((4, 4))
     return grid
-    
+
+
 def add_new(grid):
-    
+
     # get 0 pos
     zero_pos = []
     for i in range(grid.shape[0]):
         for j in range(grid.shape[1]):
             if grid[i, j] == 0:
                 zero_pos.append((i, j))
-    
-    if not zero_pos: return grid
-    
+
+    if not zero_pos:
+        return grid
+
     # 80% a 2, 20% a 4
     prob = np.random.rand()
     new_num = 2 if prob < 0.8 else 4
@@ -22,42 +26,64 @@ def add_new(grid):
     grid[pos] = new_num
     return grid
 
-    
-def bad_rollin_row(row):
-    for i in range(len(row)-1):
-        if row[i] != 0:
-            for j in range(i+1, len(row)):
-                if row[j] == row[i]:
-                    row[i] += row[j]
-                    row[j] = 0
-                    break
-                elif row[j] == 0:
-                    continue
-                else:
-                    break
-    return row
-                
+
+# def bad_rollin_row(row):
+#     for i in range(len(row) - 1):
+#         if row[i] != 0:
+#             for j in range(i + 1, len(row)):
+#                 if row[j] == row[i]:
+#                     row[i] += row[j]
+#                     row[j] = 0
+#                     break
+#                 elif row[j] == 0:
+#                     continue
+#                 else:
+#                     break
+#     return row
+
+
 # 2 2 2 2 -> 4 4 0 0
+# bugged
 def rollin_row(row):
-    for i in range(1, len(row)):
-        if row[i] == 0:
-            row = row[:i] + row[i+1:] + row[i]
-        else:
-            if row[i-1] == row[i]:
-                row[i-1] += row[i]
+    for i in range(len(row)):
+        while row[i] == 0:
+            row = row[:i] + row[i + 1 :] + [row[i]]
+        if row[i] != 0 and i != 0:
+            if row[i - 1] == row[i]:
+                row[i - 1] += row[i]
                 row[i] = 0
     return row
-    
+
+
 # print(rollin_row([2,2,2,2]))
 
+
 def rollin(grid, dir):
-    if dir == 'd':
-    
-    elif dir == 'u':
-        
-    elif dir == 'l':
+    if dir == "d":
+        for i in range(grid.shape[1]):
+            grid[:, i] = rollin_row(grid[:, i][::-1])[::-1]
+    elif dir == "u":
+        for i in range(grid.shape[1]):
+            grid[:, i] = rollin_row(grid[:, i])
+    elif dir == "l":
         for i in range(grid.shape[0]):
             grid[i] = rollin_row(grid[i])
-    elif dir == 'r':
+    elif dir == "r":
         for i in range(grid.shape[0]):
-            gird[i] = rollin_row(grid[i][::-1])[::-1]
+            grid[i] = rollin_row(grid[i][::-1])[::-1]
+
+    return grid
+
+
+def test():
+    grid = init_grid()
+    print(grid)
+    grid = add_new(grid)
+    print(grid)
+    grid = add_new(grid)
+    print(grid)
+    grid = rollin(grid, "d")
+    print(grid)
+
+
+test()
