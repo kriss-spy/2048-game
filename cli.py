fe15bad172f73d6b59916167b0beadc2adfa5d22
Wang Di <161604749+kriss-spy@users.noreply.github.com>
2024-12-03 16:25:47 +0800

    reset all changes of git test

--- cli.py
+++ cli.py
@@ -1,10 +1,3 @@
-# test git
-# test in remote edit
-# test in local edit
-# test in local edit again
-# changes in local
-# test in remote
-# test in ipad edit
 import numpy as np
 import random
 def init_grid():
